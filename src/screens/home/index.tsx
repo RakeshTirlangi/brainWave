cec022aa3d6869d5fc89f141a552a04a9eeef393
Rakesh Tirlangi <rakeshtirlangi21@gmail.com>
2024-10-20 16:02:15 +0530

    new

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -141,7 +141,7 @@ export default function Home() {
     if (canvas) {
       const response = await axios({
         method: 'post',
-        url: `${import.meta.env.VITE_API_URL}/calculate`,
+        url: 'http://localhost:8900/calculate',
         data: {
           image: canvas.toDataURL('image/png'),
           dict_of_vars: dictOfVars,
